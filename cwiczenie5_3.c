8f57c682fdfd59793874990d942e82419e595a06
aleksandra-baranowska <98693855+aleksandra-baranowska@users.noreply.github.com>
2024-10-17 14:03:35 +0200

    Update cwiczenie5_3.c

--- cwiczenie5_3.c
+++ cwiczenie5_3.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#pragma warning(disable : 4996)
 #define TYDZIEN 7
 
 int main(void)
 {
 	int dni, tygodnie, reszta;
 
+	printf("Podaj liczbe dni: ");
+	scanf_s("%d", &dni);
+
 	while (dni > 0)
 	{
-		printf("Podaj liczbe dni: ");
-		scanf_s("%d", &dni);
-
 		tygodnie = dni / TYDZIEN;
 		reszta = dni % TYDZIEN;
 		printf("%d dni to %d tygodnie, %d dni.\n", dni, tygodnie, reszta);
+
+		printf("Podaj liczbe dni: ");
+		scanf_s("%d", &dni);
 	}
 	return 0;
 }
